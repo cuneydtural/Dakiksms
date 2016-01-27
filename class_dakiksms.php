656f450411f167360c9707595f0177bc4be57f10
cuneydtural <turalcuneyd@gmail.com>
2016-01-28 00:26:48 +0200

    Signed-off-by: cuneydtural <turalcuneyd@gmail.com>

---
+++ class_dakiksms.php
@@ -0,0 +1,49 @@
+<?php
+
+class Dakiksms
+{
+    public $hash;
+    public $sitename = "http://www.dakiksms.com/api/xml_api.php";
+    public $header_type = array('Content-Type: text/xml');
+    public $password;
+    public $sender_title;
+    public $phone;
+    public $message;
+    public $xml;
+
+
+    function sendRequest()
+    {
+
+        $this->xml = '<SMS><oturum>
+            <kullanici>' . $this->hash . '</kullanici>
+            <sifre>' . $this->password . '</sifre>
+            </oturum>
+            <mesaj>
+            <baslik>' . $this->sender_title . '</baslik>
+            <metin>' . $this->message . '</metin>
+            <alicilar>' . $this->phone . '</alicilar>
+            </mesaj>
+            <karaliste>genel</karaliste>
+            <izin_link>false</izin_link>
+            <izin_telefon>false</izin_telefon>
+            </SMS>';
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $this->sitename);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $this->xml);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $this->header_type);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 120);
+        $result = curl_exec($ch);
+        return $result;
+    }
+
+}
+
+?>
---
+++ dakiksms.php
@@ -0,0 +1,24 @@
+<?php
+
+include 'class_dakiksms.php';
+
+$dakiksms = new Dakiksms();
+$dakiksms->hash = "8781881";
+$dakiksms->password = "4888dsdsqq8";
+$dakiksms->sender_title = "TITLE",
+$dakiksms->phone = "5055700909";
+$dakiksms->message = "Bu bir test mesajidir.";
+
+$sms_result = $dakiksms->sendRequest();
+
+if (substr($sms_result, 0, 2) == 'OK') {
+    // mesaj gönderildi
+    echo 'SMS gönderildi';
+
+} elseif (substr($sms_result, 0, 3) == 'ERR') {
+    echo 'SMS gönderilemedi';
+} else {
+    echo 'Bilinmeyen bir hata oluştu';
+}
+
+?>
