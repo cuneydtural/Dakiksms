2089be0c83af7efc289aba0ddf8a63e1ba355509
Cüneyd Tural <turalcuneyd@gmail.com>
2016-02-16 22:58:14 +0200

    Güncellemeler

--- dakiksms.php
+++ dakiksms.php
@@ -1,22 +1,33 @@
 <?php
 
-include 'class_dakiksms.php';
+/**
+ * Created by PhpStorm.
+ * User: turalcuneyd@gmail.com
+ * Date: 16.02.2016
+ * Time: 22:57
+ */
+
+require 'class_dakiksms.php';
 
 $dakiksms = new Dakiksms();
-$dakiksms->hash = "8781881";
-$dakiksms->password = "4888dsdsqq8";
+$dakiksms->hash = "9995522222";
+$dakiksms->password = "Qaz99aiCmnKq";
 $dakiksms->sender_title = "TITLE";
-$dakiksms->phone = "5055700909";
+$dakiksms->phone = "5394589978";
 $dakiksms->message = "Bu bir test mesajidir.";
 $sms_result = $dakiksms->sendRequest();
 
 if (substr($sms_result, 0, 2) == 'OK') {
-    // mesaj gönderildi
+    
+    // Success
     echo 'SMS gönderildi';
 
 } elseif (substr($sms_result, 0, 3) == 'ERR') {
+    
+    //Failed
     echo 'SMS gönderilemedi';
 } else {
+    // Error
     echo 'Bilinmeyen bir hata oluştu';
 }
 
